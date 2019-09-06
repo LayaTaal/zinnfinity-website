a2c47860669fb2da37521d04a82d84fc5d33b931
gendirection-jzinn <jason@zinnfinity.com>
2019-09-06 14:38:22 -0500

    Fixed blog tempalte pathing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Web Design and Development in Houston, TX | Zinnfinity Web Services`,
+    description: `WordPress websites for businesses and organizations, JavaScript solutions featuring React, and data visualization techniques for presenting complex data on the web.`,
+    author: `@taal25`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
